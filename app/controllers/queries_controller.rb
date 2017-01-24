05684735cec85c34e14c1105e50a6c78d17acb63
Arumoy Shome <arumoy.shome@gmail.com>
2017-01-24 12:45:36 -0500

    display only today's classes

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,20 +1,11 @@
 class QueriesController < ApplicationController
-  require 'uw_api/request'
-  require 'uw_api/filter'
-
-
   def new
-    @terms ||= to_json(UwApi::Request.new("terms/list").get)
+    @terms = UwApi::Request.new("terms/list").get
   end
 
   def result
-    response = to_json(UwApi::Request.new("terms/#{params[:query][:term]}/courses").get)
-    @courses = UwApi::Filter.new(response, subjects: params[:query][:subjects]).by_subject
-  end
-
-  private
-
-  def to_json(response)
-    JSON.parse(response, symbolize_names: true)
+    render :new unless params[:query][:subjects]
+    @courses = UwApi::Request.new(term: params[:query][:term],
+      subjects: params[:query][:subjects]).get_schedules
   end
 end
