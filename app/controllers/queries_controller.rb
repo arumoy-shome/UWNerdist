aad8818559289158cceba8ce882454db4efe02eb
Arumoy Shome <arumoy.shome.work@gmail.com>
2017-07-18 17:38:05 -0400

    Fixes #1. Move Request into app/models.

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,11 +1,11 @@
 class QueriesController < ApplicationController
   def new
-    @terms = UwApi::Request.new("terms/list").get
+    @terms = Request.new("terms/list").get
   end
 
   def result
     render :new unless params[:query][:subjects]
-    @courses = UwApi::Request.new(term: params[:query][:term],
+    @courses = Request.new(term: params[:query][:term],
       subjects: params[:query][:subjects]).get_schedules
   end
 end
