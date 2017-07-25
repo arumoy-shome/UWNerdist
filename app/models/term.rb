b54997aa9daba1623a9ab8ca9b84f38259470447
Arumoy Shome <arumoy.shome.work@gmail.com>
2017-07-25 00:05:09 -0400

    Introduce Term.
    This class shows information related to the term. To begin with, it
    retrieves the ids for the current years terms and returns a name for the
    term. This is done based on the following pattern:
    
    * if id ends with 9 --> Fall
    * if id ends with 5 --> Spring
    * else              --> Winter

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,6 +1,9 @@
+require 'api_helper'
+
 class Term
   def self.current_term
-    
-  end
+    current_term_id = ApiHelper.get('terms/list')[:data][:current_term]
 
+    ApiHelper.term_name(current_term_id.to_s)
+  end
 end
--- test/models/term_test.rb
+++ test/models/term_test.rb
@@ -1,16 +1,34 @@
 require 'test_helper'
+require 'json'
 
 class TermTest < ActiveSupport::TestCase
   def setup
-    stub_request(:get, "http://api.uwaterloo.ca/v2/terms/list.json").
-      with(body: %Q('data': {
-            'current_term': 1139,
-            'previous_term': 1135,
-            'next_term': 1141
-      }))
-
+    stub_request(:get, "https://api.uwaterloo.ca/v2/terms/list.json?key=#{ENV['KEY']}").
+      with(headers: stub_header ).
+      to_return(status: 200, body: stub_body, headers: {})
   end
+
   test "Term#current_term returns the current term" do
-    assert_equal "Spring 2017", Term.current_term
+    assert_equal "Fall 2017", Term.current_term
+  end
+
+  private
+
+  def stub_header
+    {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Ruby'
+    }
+  end
+
+  def stub_body
+    {
+      'data': {
+        'current_term': 1139,
+        'previous_term': 1135,
+        'next_term': 1141
+      }
+    }.to_json
   end
 end
