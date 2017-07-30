0067555dd709f0443f0f48b3cae4aa106a4673fb
Arumoy Shome <arumoy.shome.work@gmail.com>
2017-07-30 14:03:33 -0400

    Hide ApiHelper.parse from public API.
    We are not using it anywhere else apart from get so it can be made
    private.

--- app/models/api_helper.rb
+++ app/models/api_helper.rb
@@ -5,11 +5,13 @@ module ApiHelper
   BASE_URL = 'https://api.uwaterloo.ca/v2/'
   KEY = ".json?key=#{ENV['KEY']}"
 
-  def parse(response)
-    JSON.parse(response, symbolize_names: true)
-  end
-
   def get(thread)
     parse(HTTParty.get("#{BASE_URL}#{thread}#{KEY}", format: :plain))
+  end
+
+  private
+
+  def parse(response)
+    JSON.parse(response, symbolize_names: true)
   end
 end
